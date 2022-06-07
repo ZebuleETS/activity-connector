6d3600a670c4456834edf9cb624bd92451b926b1
Catarina878 <ana_catarina_s6@hotmail.com>
2022-06-06 21:29:04 -0400

    Editing dates of assignment and quiz

--- app/xml_reader.js
+++ app/xml_reader.js
@@ -14,8 +14,8 @@ function extract_tar(file_path) {
     // Check if mbz file exists, then extract to tmp directory
     if (file_path.endsWith(".mbz")) {
         try {
-            new_directory = base_path + "/" + file_path.split("/").pop().replace(".mbz","")
-            if(!fs.existsSync(new_directory)){
+            var new_directory = base_path + "/" + file_path.split("/").pop().replace(".mbz", "")
+            if (!fs.existsSync(new_directory)) {
                 fs.mkdirSync(new_directory)
             }
             tar.x({
@@ -30,16 +30,44 @@ function extract_tar(file_path) {
     }
 }
 
-function fetch_activities(file_path){
+
+function fetch_quiz_info(file_path, directory) {
+    var data = fs.readFileSync(file_path + directory + "/quiz.xml", "utf-8")
+    var quiz_info
+    xml2js.parseString(data, function (err, data) {
+        quiz_info = {
+            timeopen: data['activity']['quiz'][0]['timeopen'][0],
+            timeclose: data['activity']['quiz'][0]['timeopen'][0],
+        }
+    })
+
+    return quiz_info
+}
+
+
+function fetch_assign_info(file_path, directory) {
+    var data = fs.readFileSync(file_path + directory + "/assign.xml", "utf-8")
+    var assign_info
+    xml2js.parseString(data, function (err, data) {
+        assign_info = {
+            duedate: data['activity']['assign'][0]['duedate'][0],
+            allowsubmissionsfromdate: data['activity']['assign'][0]['allowsubmissionsfromdate'][0],
+        }
+    })
+
+    return assign_info
+}
+
+function fetch_activities(file_path) {
     var activities = []
     var xml_data = fs.readFileSync(file_path + "moodle_backup.xml", "utf-8")
-    xml2js.parseString(xml_data, function(err, data){
-        for (var obj of data['moodle_backup']['information'][0]['contents'][0]['activities'][0]['activity']){
-            switch(obj.modulename[0]){
+    xml2js.parseString(xml_data, function (err, data) {
+        for (var obj of data['moodle_backup']['information'][0]['contents'][0]['activities'][0]['activity']) {
+            switch (obj.modulename[0]) {
                 case 'quiz':
                     quiz_info = fetch_quiz_info(file_path, obj.directory[0])
                     activities.push(new MoodleQuiz(
-                        obj.title[0], obj.moduleid[0], obj.sectionid[0], obj.modulename[0], obj.directory[0], 
+                        obj.title[0], obj.moduleid[0], obj.sectionid[0], obj.modulename[0], obj.directory[0],
                         quiz_info.timeopen, quiz_info.timeclose
                     ))
                     break;
@@ -57,40 +85,16 @@ function fetch_activities(file_path){
             }
         }
     })
-    console.log(activities)
+    //console.log(activities)
     return activities
 }
+// var path = "data/backup-moodle2-course-1677-s20143-log792-09-20151102-1508-nu.mbz"
+// var new_path = "tmp/backup-moodle2-course-1677-s20143-log792-09-20151102-1508-nu/"
 
+// extract_tar(path)
+// fetch_activities(new_path)
 
-function fetch_quiz_info(file_path, directory){
-    var data = fs.readFileSync(file_path + directory + "/quiz.xml", "utf-8")
-    var quiz_info
-    xml2js.parseString(data, function(err, data){
-        quiz_info = {
-            timeopen: data['activity']['quiz'][0]['timeopen'][0],
-            timeclose: data['activity']['quiz'][0]['timeopen'][0],
-        }
-    })
-
-    return quiz_info
+module.exports = {
+    extract_tar: extract_tar,
+    fetch_activities: fetch_activities
 }
-
-
-function fetch_assign_info(file_path, directory){
-    var data = fs.readFileSync(file_path + directory + "/assign.xml", "utf-8")
-    var assign_info
-    xml2js.parseString(data, function(err, data){
-        assign_info = {
-            duedate: data['activity']['assign'][0]['duedate'][0],
-            allowsubmissionsfromdate: data['activity']['assign'][0]['allowsubmissionsfromdate'][0],
-        }
-    })
-
-    return assign_info
-}
-
-var path = "data/backup-moodle2-course-1677-s20143-log792-09-20151102-1508-nu.mbz"
-var new_path = "tmp/backup-moodle2-course-1677-s20143-log792-09-20151102-1508-nu/"
-
-extract_tar(path)
-fetch_activities(new_path)
