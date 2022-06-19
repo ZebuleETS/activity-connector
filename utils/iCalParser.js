54f86489855c793f4356867f299fe81306e2ed63
mayorathan <mayorathan@outlook.com>
2022-06-19 11:04:02 -0400

    changed calendar parsing

--- utils/iCalParser.js
+++ utils/iCalParser.js
@@ -1,15 +1,28 @@
 // import ical
 const ical = require("node-ical");
 
+const BASE_URL = "https://portail.etsmtl.ca/ICal/SeancesCours?"
+
 class iCalParser {
-  constructor(url) {
-    this.url = url;
+  constructor(typeact, symbol, group, year, semesterSeason) {
+    this.typeact = typeact;
+    this.symbol = symbol;
+    this.group = group;
+    this.year = year;
+    this.semesterSeason = semesterSeason;
   }
 
   parse = async () => {
-    const events = await ical.async.fromURL(this.url);
+    const params = new URLSearchParams(`typeact=${this.typeact}&` + 
+      `Sigle=${this.symbol}&` + 
+      `Groupe=${this.group}&` + 
+      `Session=${this.year + this.semesterSeason}&`
+    );
 
-    for (const event of Object.values(events)) {
+    const url = await ical.async.fromURL(BASE_URL + params.toString());
+    
+    for (const event of Object.values(url)) {
+      console.log(event);
       console.log(
         "Summary: " + event.summary +
         "\nDescription: " + event.description +
